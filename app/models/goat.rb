d678f5ee6a5d3b0279ebc65206f4d382c909aebe
FabienLH <133055509+CartonSlip@users.noreply.github.com>
2024-08-12 16:15:24 +0200

    Validates goats ok

--- app/models/goat.rb
+++ app/models/goat.rb
@@ -1,3 +1,7 @@
 class Goat < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, uniqueness: { scope: :user }
+  validates :race, presence: true
+  validates :description, presence: true, length: { minumum:10, maximum:500 }
+  validates :photo, presence: true
 end
