257a6ac4193afa2df42f605a85639d8f24564388
david chardon <syhnes@gmail.com>
2024-08-12 16:43:12 +0200

    add details users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true, uniqueness: { scope: :first_name }
   validates :address, presence: true
-  validates :description, length: { minimum: 10, maximum: 500 }
 end
