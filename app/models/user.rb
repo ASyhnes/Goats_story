c1021b737b666cf501fb6893494383c2254ee39e
david chardon <syhnes@gmail.com>
2024-08-12 17:03:07 +0200

    comment validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true, uniqueness: { scope: :first_name }
   validates :address, presence: true
-  validates :photo, presence: true
-
+  # validates :photo, presence: true
 end
