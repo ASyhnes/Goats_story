3b9be21c5ada2cb84f3f96bf08acecfc1d08a4a2
FabienLH <133055509+CartonSlip@users.noreply.github.com>
2024-08-12 17:12:34 +0200

    comment line goats photos presense true

--- app/models/goat.rb
+++ app/models/goat.rb
@@ -3,5 +3,5 @@ class Goat < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :user }
   validates :race, presence: true
   validates :description, presence: true, length: { minumum:10, maximum:500 }
-  validates :photo, presence: true
+  # validates :photo, presence: true
 end
