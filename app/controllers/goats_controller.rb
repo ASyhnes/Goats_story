09d5180dd0255ce561f8e77d4ddf77fdd70be35b
Alexandre Rudeau <contact@arudeau.fr>
2024-08-13 14:16:06 +0200

    Feature Create Goat, API Cloudinary on the model

--- app/controllers/goats_controller.rb
+++ app/controllers/goats_controller.rb
@@ -1,4 +1,26 @@
 class GoatsController < ApplicationController
+  def new
+    @goat = Goat.new
+  end
+
+  def create
+    @goat = Goat.new(goat_params)
+    @goat.user = current_user
+
+    if @goat.save
+      # redirect_to profil_path(@current_user)
+      redirect_to goats_path, notice: "Votre chêvre a bien été ajoutée"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def goat_params
+    params.require(:goat).permit(:name, :race, :description, photos: [])
+  end
+
 
   def show
     @goat = Goat.find(params[:id])
